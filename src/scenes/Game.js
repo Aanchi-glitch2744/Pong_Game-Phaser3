85bc9e113c9747b568519800ec72317d3a952808
Aanchi-glitch2744 <aanchalkumari2744@gmail.com>
2021-10-24 12:40:45 +0530

    Added font from Webfontloader

--- src/scenes/Game.js
+++ src/scenes/Game.js
@@ -1,5 +1,7 @@
 import Phaser from 'phaser'
 
+import WebFontFile from './WebFontFile'
+
 class Game extends Phaser.Scene
 {
     init(){
@@ -8,16 +10,17 @@ class Game extends Phaser.Scene
         this.leftScore = 0
         this.rightScore = 0
     }
-    preload()
+    preload()   //Uses webfontloader
     {
-
+        const fonts = new WebFontFile(this.load, 'Press Start 2P')
+        this.load.addFile(fonts)
     }
 
     create()
     {
         this.physics.world.setBounds(-100, 0, 1000, 500)
 
-        // this.add.text(400, 250, 'Game') //check display.
+       
         this.ball= this.add.circle(400, 250, 10, 0xffffff, 1) //create a white ball in the centre of screen
         this.physics.add.existing(this.ball) //add physics to ball
         this.ball.body.setBounce(1, 1)
@@ -25,16 +28,10 @@ class Game extends Phaser.Scene
         this.ball.body.setCollideWorldBounds(true, 1, 1) //Added ball bouncing capabilities off the border/bounds of screen 
 
         this.resetBall()
-        // const angle = Phaser.Math.Between(0,360)
-        // const vec = this.physics.velocityFromAngle(angle, 200) //200 is speed
-
-        //ball.body.setVelocity(200, 200) //set velocity for the body
-        // this.ball.body.setVelocity(Phaser.Math.Between(-200, 200), Phaser.Math.Between(-200, 200)) //randomise ball movement on screen in x and y direction.
-        // this.ball.body.setVelocity(vec.x, vec.y)   //moved upto here to resetBall()
 
         this.paddleLeft = this.add.rectangle(50, 250, 30, 100, 0xffffff, 1)  //add a player paddle in left side of screen.
-        this.physics.add.existing(this.paddleLeft, true) //added physics so that it reacts to surroundings and ball.
-        //paddleLeft.body.setBounce(1, 1) //Bounce off the body of paddle left.
+        this.physics.add.existing(this.paddleLeft, true) //added physics to surroundings and ball.
+        
         
         this.paddleRight = this.add.rectangle(750, 250, 30, 100, 0xffffff, 1)
         this.physics.add.existing(this.paddleRight, true)
@@ -43,14 +40,15 @@ class Game extends Phaser.Scene
         this.physics.add.collider(this.paddleRight, this.ball)
         
         const scoreStyle =  {
-            fontSize: 48
+            fontSize: 48,
+            fontFamily: '"Press Start 2P"' //use both quites if there is a spacing in font name.
         }
         this.leftScoreLabel = this.add.text(300, 125, '0', scoreStyle) //Adding score
             .setOrigin(0.5, 0.5) 
         this.rightScoreLabel = this.add.text(500, 325, '0', scoreStyle)
         .setOrigin(0.5, 0.5)
 
-        this.cursors= this.input.keyboard.createCursorKeys() //Make Up and down keys //takes input from keyboard for Up Left Right Down, shapebar and shift.
+        this.cursors= this.input.keyboard.createCursorKeys() //Make Up and down keys.
     
 
     }
@@ -60,34 +58,24 @@ class Game extends Phaser.Scene
         const body = this.paddleLeft.body
 
         if (this.cursors.up.isDown){
-            // console.log('up pressed')
-            // body.setVelocityY(-100)
             this.paddleLeft.y -= 10 //go up
             body.updateFromGameObject() //To update the physics box.
 
         }
         else if (this.cursors.down.isDown){
-            // console.log('down pressed')
-            // body.setVelocityY(100)
             this.paddleLeft.y += 10 //go down
-            body.updateFromGameObject() //To update the physics box.
+            body.updateFromGameObject() 
 
         }
-        // else{
-        //     this.paddleLeft.setVelocityY(0)
-        // }
         const diff = this.ball.y - this.paddleRight.y
-        // console.log(diff)
-        if(Math.abs(diff) < 10){ //to make the right paddle move smoothly without jitter
-            return 
-        }
+        // if(Math.abs(diff) < 10){ //to make the right paddle move smoothly without jitter
+        //     return 
+        // }
 
         const aiSpeed = 3
 
         if(diff< 0){
             //ball is above the paddle
-            // this.paddleRight.y -= 10
-            // this.paddleRight.body.updateFromGameObject()
             this.paddleRightVelocity.y = -aiSpeed
             if (this.paddleRightVelocity.y < -10){
                 this.paddleRightVelocity.y = -10
@@ -95,8 +83,6 @@ class Game extends Phaser.Scene
         }
         else if(diff > 0){
             //ball is below the paddle
-            // this.paddleRight.y += 10
-            // this.paddleRight.body.updateFromGameObject()
             this.paddleRightVelocity.y = aiSpeed
             if (this.paddleRightVelocity.y > 10){
                 this.paddleRightVelocity.y = 10
